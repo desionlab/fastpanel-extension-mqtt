e95350d4dbb2d9025e84fbc821076a959bff7de9
Fenixphp <fenixphp@gmail.com>
2019-01-16 09:16:52 +0300

    Update core modules.

--- src/Commands/Setup.ts
+++ src/Commands/Setup.ts
@@ -6,6 +6,7 @@
  * @license   MIT
  */
 
+import { EOL } from 'os';
 import Winston from 'winston';
 import { Cli } from '@fastpanel/core';
 
@@ -24,13 +25,20 @@ export class Setup extends Cli.CommandDefines {
     .command('fastpanel/mqtt setup', 'Configure mqtt components.')
     .option('-e, --env', 'Save as current environment settings.')
     .option('-f, --force', 'Forced command running.')
-    .option('-y, --yes', 'Assume yes if prompted.')
     .visible(false)
     .action((args: {[k: string]: any}, options: {[k: string]: any}, logger: Winston.Logger) => {
       return new Promise(async (resolve, reject) => {
-        logger.debug('@fastpanel/mqtt setup');
-        logger.debug(args);
-        logger.debug(options);
+        /* Info message. */
+        logger.info(`${EOL}Configure mqtt components.`);
+
+        if (!this.config.get('Ext/Mqtt', false) || options.force) {
+          
+        } else {
+          /* Info message. */
+          logger.info(` Everything is already configured. ${EOL}`);
+        }
+
+        /* Command complete. */
         resolve();
       });
     });
